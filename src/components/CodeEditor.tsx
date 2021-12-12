8b22774c00c258f5e7d4a49b9f26516a7d5962c9
Tomás <tomascatena@gmail.com>
2021-12-12 14:57:04 +0100

    add prettier to format the code

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,39 +1,63 @@
-import React, { FC } from 'react';
+import React, { FC, useRef } from 'react';
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
 interface CodeEditorProps {
-	initialValue: string;
-	onChange(value: string): void;
+  initialValue: string;
+  onChange(value: string): void;
 }
 
 const CodeEditor: FC<CodeEditorProps> = ({ initialValue, onChange }) => {
-	const onEditorDidMount: EditorDidMount = (getCurrentValue, monacoEditor) => {
-		monacoEditor.onDidChangeModelContent(() => {
-			onChange(getCurrentValue());
-		});
+  const editorRef = useRef<any>();
 
-		monacoEditor.getModel()?.updateOptions({tabSize: 2})
-	};
+  const onEditorDidMount: EditorDidMount = (getCurrentValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
 
-	return (
-		<MonacoEditor
-			editorDidMount={onEditorDidMount}
-			value={initialValue}
-			language='javascript'
-			height='500px'
-			theme='dark'
-			options={{
-				wordWrap: 'on',
-				minimap: { enabled: false },
-				showUnused: true,
-				folding: false,
-				lineNumbersMinChars: 3,
-				fontSize: 16,
-				scrollBeyondLastLine: false,
-				automaticLayout: true
-			}}
-		/>
-	);
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getCurrentValue());
+    });
+
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+  };
+
+  const onFormatClick = () => {
+    const unformatted = editorRef.current.getModel().getValue();
+
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+
+    editorRef.current.setValue(formatted);
+  };
+
+  return (
+    <>
+      <button onClick={onFormatClick}>Format</button>
+
+      <MonacoEditor
+        editorDidMount={onEditorDidMount}
+        value={initialValue}
+        language='javascript'
+        height='500px'
+        theme='dark'
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: true,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </>
+  );
 };
 
 export default CodeEditor;
