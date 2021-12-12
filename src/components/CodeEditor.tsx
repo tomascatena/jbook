72995e1c1a22d57dccbe99d673c9fa1b1d885c25
Tomás <tomascatena@gmail.com>
2021-12-12 14:14:21 +0100

    configure monaco editor

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -2,7 +2,23 @@ import React, { FC } from 'react';
 import MonacoEditor from '@monaco-editor/react';
 
 const CodeEditor: FC = () => {
-	return <MonacoEditor height='500px' />;
+	return (
+		<MonacoEditor
+			language='javascript'
+			height='500px'
+			theme='dark'
+			options={{
+				wordWrap: 'on',
+				minimap: { enabled: false },
+				showUnused: true,
+				folding: false,
+				lineNumbersMinChars: 3,
+				fontSize: 16,
+				scrollBeyondLastLine: false,
+				automaticLayout: true
+			}}
+		/>
+	);
 };
 
 export default CodeEditor;
