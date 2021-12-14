f4af7f333db11f12f6b969026cb789551a03b250
Tomás <tomascatena@gmail.com>
2021-12-14 13:34:06 +0100

    fix resize issue

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,8 +1,25 @@
 import React, { FC, useEffect, useRef } from 'react';
+import { styled } from '@mui/system';
 
-interface Props {
-  code: string;
-}
+const IframeContainer = styled('div')(({ theme }) => ({
+  position: 'relative',
+  height: '100%',
+
+  '.react-draggable-transparent-selection &:after': {
+    content: '""',
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    backgroundColor: 'red',
+  },
+
+  iframe: {
+    height: '100%',
+    backgroundColor: '#fff',
+  },
+}));
 
 const html = `
 <html>
@@ -27,6 +44,10 @@ const html = `
 </html>
 `;
 
+interface Props {
+  code: string;
+}
+
 const Preview: FC<Props> = ({ code }) => {
   const iframe = useRef<any>();
 
@@ -39,13 +60,14 @@ const Preview: FC<Props> = ({ code }) => {
   }
 
   return (
-    <iframe
-      style={{ backgroundColor: '#fff' }}
-      title='preview'
-      sandbox='allow-scripts'
-      srcDoc={html}
-      ref={iframe}
-    />
+    <IframeContainer>
+      <iframe
+        title='preview'
+        sandbox='allow-scripts'
+        srcDoc={html}
+        ref={iframe}
+      />
+    </IframeContainer>
   );
 };
 
