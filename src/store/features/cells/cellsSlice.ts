dec0995477128777f775e36f04c1e2b073bf85d4
Tomás <tomascatena@gmail.com>
2021-12-18 13:52:30 +0100

    add logic for moveCell, deleteCell, insertCellBefore, insertCellAfter

--- src/store/features/cells/cellsSlice.ts
+++ src/store/features/cells/cellsSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { CellTypes, Cell, Direction } from '../../cell';
+import { v4 as uuidv4 } from 'uuid';
 
 export interface CellsState {
   data: { [key: string]: Cell };
@@ -22,16 +23,72 @@ export const cellsSlice = createSlice({
     moveCell: (
       state,
       action: PayloadAction<{ id: string; direction: Direction }>
-    ) => {},
-    deleteCell: (state, action: PayloadAction<string>) => {},
+    ) => {
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return; // invalid state update
+      }
+
+      [
+        state.order[index], //
+        state.order[targetIndex],
+      ] = [
+        state.order[targetIndex], //
+        state.order[index],
+      ];
+    },
+    deleteCell: (state, action: PayloadAction<string>) => {
+      delete state.data[action.payload];
+
+      state.order = state.order.filter((id) => id !== action.payload);
+    },
     insertCellBefore: (
       state,
-      action: PayloadAction<{ id: string; type: CellTypes }>
-    ) => {},
+      action: PayloadAction<{ id: string | null; type: CellTypes }>
+    ) => {
+      const { type } = action.payload;
+
+      const cell: Cell = {
+        id: uuidv4(),
+        type,
+        content: '',
+      };
+
+      state.data[cell.id] = cell;
+
+      const index = state.order.findIndex((id) => id === action.payload.id);
+
+      if (index < 0) {
+        state.order.push(cell.id);
+      } else {
+        state.order.splice(index, 0, cell.id);
+      }
+    },
     insertCellAfter: (
       state,
       action: PayloadAction<{ id: string; type: CellTypes }>
-    ) => {},
+    ) => {
+      const { type } = action.payload;
+
+      const cell: Cell = {
+        id: uuidv4(),
+        type,
+        content: '',
+      };
+
+      state.data[cell.id] = cell;
+
+      const index = state.order.findIndex((id) => id === action.payload.id);
+
+      if (index < 0 || index > state.order.length - 1) {
+        state.order.push(cell.id);
+      } else {
+        state.order.splice(index + 1, 0, cell.id);
+      }
+    },
     updateCell: (
       state,
       action: PayloadAction<{ id: string; content: string }>
