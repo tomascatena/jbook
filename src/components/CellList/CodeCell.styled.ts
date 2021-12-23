f7f7a7e50be95ac11b29b7ed0a2aad0fadc0f779
Tomás Catena <tomascatena@gmail.com>
2021-12-23 21:19:21 +0100

    fix bottom edge snap

--- src/components/CellList/CodeCell.styled.ts
+++ src/components/CellList/CodeCell.styled.ts
@@ -2,6 +2,9 @@ import { styled } from '@mui/system';
 import Box from '@mui/material/Box';
 
 export const CellListWrapper = styled(Box)(({ theme }) => ({
-  marginLeft: theme.spacing(3),
-  marginRight: theme.spacing(3),
+  margin: `0 ${theme.spacing(3)}`,
+
+  '.react-draggable-transparent-selection &': {
+    marginBottom: '100vh',
+  },
 }));
