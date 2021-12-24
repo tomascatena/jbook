4d8ac2113dde57286442bdabf105e88c07b5ea0c
Tomás Catena <tomascatena@gmail.com>
2021-12-24 19:02:22 +0100

    merge

--- packages/cli/dist/index.js
+++ packages/cli/dist/index.js
@@ -5,4 +5,3 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const local_api_1 = __importDefault(require("local-api"));
 (0, local_api_1.default)();
-//# sourceMappingURL=index.js.map
--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const serve = () => {
-    console.log('Server started...');
+    console.log('Server is listening!');
 };
 exports.default = serve;
 //# sourceMappingURL=index.js.map
