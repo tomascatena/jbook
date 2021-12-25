cca5cea30a1aa85b89e2ac3b50129edce101290b
Tomás Catena <tomascatena@gmail.com>
2021-12-25 22:38:44 +0100

    serve build from local-client in local-api

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,14 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const express_1 = __importDefault(require("express"));
-const http_proxy_middleware_1 = require("http-proxy-middleware");
 const serve = (port, filename, dir) => {
     const app = (0, express_1.default)();
-    app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
-        target: 'http://localhost:3000',
-        ws: true,
-        logLevel: 'debug',
-    }));
+    app.use(express_1.default.static('../node_modules/local-client/build'));
+    // app.use(
+    //   createProxyMiddleware({
+    //     target: 'http://localhost:3000',
+    //     ws: true,
+    //     logLevel: 'debug',
+    //   })
+    // );
     return new Promise((resolve, reject) => {
         app.listen(port, resolve).on('error', reject);
     });
