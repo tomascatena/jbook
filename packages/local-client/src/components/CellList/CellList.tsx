0c2c236ccd2aae88251cbaf5a964556fc16371b1
Tomás Catena <tomascatena@gmail.com>
2021-12-26 16:55:47 +0100

    add logic to fetch list of cells and save a list of cells

--- packages/local-client/src/components/CellList/CellList.tsx
+++ packages/local-client/src/components/CellList/CellList.tsx
@@ -1,12 +1,20 @@
-import React, { FC, Fragment } from 'react';
+import React, { FC, Fragment, useEffect } from 'react';
 import { useTypedSelector } from '../../hooks';
 import CellListItem from '../CellListItem/CellListItem';
 import AddCell from '../AddCell/AddCell';
 import { CellListWrapper } from './CodeCell.styled';
+import { useAppDispatch } from '../../hooks';
+import { fetchCells } from '../../store/features/cells/cellsSlice.thunk';
 
 interface Props {}
 
 const CellList: FC = (props: Props) => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCells());
+  }, []);
+
   const { data, order } = useTypedSelector((state) => state.cells);
 
   const cellsList = order.map((id) => data[id]);
--- packages/local-client/src/store/features/cells/cellsSlice.ts
+++ packages/local-client/src/store/features/cells/cellsSlice.ts
@@ -1,12 +1,15 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction, SerializedError } from '@reduxjs/toolkit';
 import { CellTypes, Cell, Direction } from '../../cell';
 import { v4 as uuidv4 } from 'uuid';
+import { fetchCells, saveCells } from './cellsSlice.thunk';
 
 export interface CellsState {
   data: { [key: string]: Cell };
   loading: boolean;
-  error: string | null;
+  error: string | null | SerializedError;
   order: string[];
+  currentRequestId: string | undefined;
+  saveError: string | null | SerializedError;
 }
 
 const initialState: CellsState = {
@@ -14,6 +17,8 @@ const initialState: CellsState = {
   loading: false,
   error: null,
   order: [],
+  currentRequestId: undefined,
+  saveError: null,
 };
 
 export const cellsSlice = createSlice({
@@ -75,6 +80,58 @@ export const cellsSlice = createSlice({
 
       state.data[id].content = content;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCells.pending, (state, action) => {
+        if (state.loading === false) {
+          state.loading = true;
+          state.error = null;
+          state.currentRequestId = action.meta.requestId;
+        }
+      })
+      .addCase(fetchCells.fulfilled, (state, action) => {
+        const { requestId } = action.meta;
+        if (state.loading === true && state.currentRequestId === requestId) {
+          state.loading = false;
+          state.currentRequestId = undefined;
+          state.order = action.payload.map((cell) => cell.id);
+          state.data = action.payload.reduce((accumulator, cell) => {
+            accumulator[cell.id] = cell;
+            return accumulator;
+          }, {} as CellsState['data']);
+        }
+      })
+      .addCase(fetchCells.rejected, (state, action) => {
+        const { requestId } = action.meta;
+        if (state.loading === true && state.currentRequestId === requestId) {
+          state.loading = false;
+          state.error = action.error;
+          state.currentRequestId = undefined;
+        }
+      })
+      .addCase(saveCells.pending, (state, action) => {
+        if (state.loading === false) {
+          state.loading = true;
+          state.saveError = null;
+          state.currentRequestId = action.meta.requestId;
+        }
+      })
+      .addCase(saveCells.fulfilled, (state, action) => {
+        const { requestId } = action.meta;
+        if (state.loading === true && state.currentRequestId === requestId) {
+          state.loading = false;
+          state.currentRequestId = undefined;
+        }
+      })
+      .addCase(saveCells.rejected, (state, action) => {
+        const { requestId } = action.meta;
+        if (state.loading === true && state.currentRequestId === requestId) {
+          state.loading = false;
+          state.saveError = action.error;
+          state.currentRequestId = undefined;
+        }
+      });
   },
 });
 
